3b89c7c9366b7274003d22cc811b456efe9537eb
Flinpow <thalesgc2011@hotmail.com>
2023-10-15 19:53:36 -0300

    ajustes nos metodos de buscar arquivos

--- IndexerProject/src/main/java/com/mycompany/indexerproject/Indexer.java
+++ IndexerProject/src/main/java/com/mycompany/indexerproject/Indexer.java
@@ -4,12 +4,16 @@
  */
 package com.mycompany.indexerproject;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.AccessDeniedException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,28 +22,29 @@ public class Indexer {
     public static final String FREQWORD_OPERATION = "freq-word";
     public static final String SEARCH_OPERATION = "search";
      public static void main(String[] args) {
-
+         
+    //Veriica o tipo da operação passada no primeiro parâmetro de entrada - arg[0]
        try {
         if (args[0].contains(FREQ_OPERATION)){
             freqNFile(args[1], args[2]);
         } else if(args[0].contains(FREQWORD_OPERATION)) {
-            
+            freqWordNFile(args[1], args[2]);    
         } else if (args[0].contains(SEARCH_OPERATION)) {
-            
+            searchTermNFile(args[1], args[2]);
         }else {
             System.out.println("Forneça um argumento válido");
         }
-       }catch(Exception e) {
-           System.out.println(e);
+       }catch(ArrayIndexOutOfBoundsException e) {
            System.out.println("Forneça todos os 3 argumentos para a execução correta do indexador");
        }
     }
     
-    private static void freqNFile(String n, String file) throws IOException { 
+    private static void freqNFile(String n, String filePath) { 
         System.out.println("Iniciando operação --freq N ARQUIVO");
         try{
+           // apenas testando a leitura e a contagem de palavras dentro do arquivo. ainda não faz o procedimento necessário pro trabalho
            int wordsNumber = Integer.valueOf(n);
-           String content = readFile(file, StandardCharsets.UTF_8);
+           String content = readFile(filePath, StandardCharsets.UTF_8);
            
            Pattern pattern = Pattern.compile("\\b\\w+\\b");
            Matcher matcher = pattern.matcher(content);
@@ -52,20 +57,60 @@ public class Indexer {
             System.out.println("o número de palavras no arquivo é: " + count);
         } catch(NumberFormatException e) {
             System.out.println("Forneça um número válido para a operação de busca"); 
+        } catch (AccessDeniedException e ) {
+            System.out.println("Para desta operação forneca o caminho para um arquivo de texto");
         }
     }
         
     
-    private static void freqWordNFile() {
+    private static void freqWordNFile(String word, String filePath) {
         System.out.println("Operação --freq-word PALAVRA ARQUIVO");
+        try{
+           // apenas testando a leitura e a contagem de palavras dentro do arquivo. ainda não faz o procedimento necessário pro trabalho
+           String content = readFile(filePath, StandardCharsets.UTF_8);
+           
+           Pattern pattern = Pattern.compile("\\b\\w+\\b");
+           Matcher matcher = pattern.matcher(content);
+           int count = 0;
+           
+           while(matcher.find()) {
+               count++;
+           }
+           
+            System.out.println("o número de palavras no arquivo é: " + count);
+        } catch (AccessDeniedException e ) {
+            System.out.println("Para desta operação forneca o caminho para um arquivo de texto");
+        }
+        
     }
     
-    private static void searchTermNFile() {
+    private static void searchTermNFile(String word, String filePath) {
         System.out.println("Operação --search TERMO ARQUIVO");
+        File[] files = getFiles(filePath);
+        if(files != null) {
+          System.out.println(files.length + " arquivos presentes no diretório " + filePath);  
+        } else {
+            System.out.println("Diretório inexsistente");
+        }
     }
-    static String readFile(String path, Charset encoding) throws IOException 
-{
-    byte[] encoded = Files.readAllBytes(Paths.get(path));
-    return encoding.decode(ByteBuffer.wrap(encoded)).toString();
-}
+    
+    //retorna o conteúdo do arquivo em formato de String. utilizando nos casos de --freq e --freq-word
+    static String readFile(String path, Charset encoding) throws AccessDeniedException {
+        byte[] encoded = null;
+        try {
+            encoded = Files.readAllBytes(Paths.get(path));
+        } catch (IOException ex) {
+            System.out.println("Não foi possivel ler o arquivo");
+        }
+        return encoding.decode(ByteBuffer.wrap(encoded)).toString();
+    }
+
+    // traz todos os arquivos presentes no diretório passado por parâmetro na operação de --search
+    private static File[] getFiles(String pathName) {
+        File[] arquivos = null;
+        
+        File path = new File(pathName);
+        arquivos = path.listFiles();
+        return arquivos;
+    }
 }
